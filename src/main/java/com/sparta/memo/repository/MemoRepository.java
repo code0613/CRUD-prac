833e051207b7dfa97aeefaf00f1f59e3d0878a2c
code0613 <vhxmfltm3000@naver.com>
2022-11-30 18:17:48 +0900

    feat: Add Project Repository Repository 파일 추가

---
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -0,0 +1,15 @@
+package com.sparta.memo.repository;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.entity.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findAllByOrderByModifiedAtDesc();
+
+    Optional<Memo> findByIdAndPassword(Long id, String password);
+
+}
